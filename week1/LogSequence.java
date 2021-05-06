68a9724de581f39ac1ac179a37ff155e9d9010ff
Aidana Akimova <56923613+adnakimova@users.noreply.github.com>
2021-05-06 21:37:15 +0300

    Add files via upload

---
+++ week1/LogSequence.java
@@ -0,0 +1,17 @@
+package Week1;
+
+import java.util.Scanner;
+
+public class LogSequence {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        for(int i = 1; i <= y; i++){
+            System.out.print(i + " ");
+            if(i%x == 0)
+                System.out.println();
+
+        }
+    }
+}
