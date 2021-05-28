610cf53933debec2a2de5aaea6ddf78c22576d75
Aidana Akimova <56923613+adnakimova@users.noreply.github.com>
2021-05-28 20:57:13 +0300

    Update pojoclass.java

--- week4/pojoclass.java
+++ week4/pojoclass.java
@@ -1,52 +1,65 @@
-package week4;
 
 public class Car {
     private String brand;
     private int year;
     private int price;
     private String country;
+    private int idNum;
 
-    public Car(String brand, int price){
+    public Car(String brand, int price) {
         this.brand = brand;
         this.price = price;
     }
-    public Car(String brand, int year,int price,String country){
+
+    public Car(int idNum, String brand, int year, int price, String country) {
+        this.idNum = idNum;
         this.brand = brand;
         this.price = price;
         this.year = year;
         this.country = country;
 
     }
-    public String getBrand(){
+
+    public String getBrand() {
         return this.brand;
     }
-    public void setBrand(String brand){
+
+    public void setBrand(String brand) {
         this.brand = brand;
     }
 
-    public int getPrice(){
-        return this.price;
-    }
-    public void setPrice(int price){
-        if(price > 0)
+
+    public void setPrice(int price) {
+        if (price > 0)
             this.price = price;
         else
             System.out.println("Price can not be less than 0");
     }
-    public int getYear(){
+    public int getPrice () {
         return this.price;
     }
-    public void setYear(int year){
-        if(year > 0)
+   
+    public int getYear () {
+        return this.price;
+    }
+    public void setYear ( int year){
+        if (year > 0)
             this.year = year;
         else
             System.out.println("Year can not be less than 0");
     }
-    public String getCountry(){
+    public String getCountry () {
         return this.country;
     }
-    public void setCountry(String country){
+    public void setCountry (String country){
         this.country = country;
+    }
+    public int getIdNum () {
+        return idNum;
+    }
+
+    public void setIdNum ( int idNum){
+        this.idNum = idNum;
     }
 
     @Override
@@ -56,14 +69,19 @@ public class Car {
                 ", year=" + year +
                 ", price=" + price +
                 ", country='" + country + '\'' +
+                ", idNum=" + idNum +
                 '}';
     }
 
-    public static void main(String args[]) {
-        Car car1 = new Car("Toyota",15000);
-        car1.setYear(2018);
-        car1.setCountry("Japan");
-        System.out.println(car1);
+    public static void main (String args[]){
+            Car car1 = new Car("Toyota", 15000);
+            car1.setYear(2018);
+            car1.setCountry("Japan");
+            car1.setIdNum(1);
+            System.out.println(car1);
 
-    }
+        }
+    
 }
+
+   
